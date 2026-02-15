d5d28898625bfa9131d3335329d389d2b7965a86
JimmyLv <jimmy.jinglv@gmail.com>
2026-02-15 23:57:59 +0800

    Simplify formula

--- Formula/bibigpt.rb
+++ Formula/bibigpt.rb
@@ -1,8 +1,6 @@
 # BibiGPT Desktop - AI Video/Audio Summary Assistant
 # https://bibigpt.co
 
-require "os"
-
 class Bibigpt < Formula
   desc "BibiGPT - AI Video/Audio Summary Assistant"
   homepage "https://bibigpt.co"
@@ -14,31 +12,24 @@ class Bibigpt < Formula
     regex(/(\d+\.\d+\.\d+)/)
   end
   
-  # Auto-select URL based on architecture
+  version "4.252.4"
+  
   if OS.mac? && Hardware::CPU.arm?
     url "https://bibigpt-apps.oss-cn-beijing.aliyuncs.com/desktop-releases/BibiGPT-4.252.4-darwin-aarch64.app.tar.gz"
   elsif OS.mac?
     url "https://bibigpt-apps.oss-cn-beijing.aliyuncs.com/desktop-releases/BibiGPT-4.252.4-darwin-x86_64.app.tar.gz"
   end
   
-  version "4.252.4"
   sha256 "REPLACE_WITH_ACTUAL_SHA256"
   license "MIT"
   bottle :unneeded
   
   def install
-    # Extract tar.gz
     tarball = Dir["*.tar.gz"].first
     system "tar", "-xzf", tarball if tarball
-    
-    # Copy app to Applications
     cp_r "BibiGPT.app", "/Applications/"
-    
-    # Create CLI symlink if exists
     cli_path = "#{appdir}/BibiGPT.app/Contents/MacOS/BibiGPT"
-    if File.exist?(cli_path)
-      bin.install_symlink cli_path => "bibi"
-    end
+    bin.install_symlink cli_path => "bibi" if File.exist?(cli_path)
   end
   
   test do
